ce6849673dcb2ad9218edc92a934bf8d6984f0f3
deep <boliyadeep2003@gmail.com>
2024-04-23 01:48:19 +0530

    servo-23 not working

--- Motor_control/Pi_gpio/pi_mot.py
+++ Motor_control/Pi_gpio/pi_mot.py
@@ -21,24 +21,22 @@ import pigpio
 pi = pigpio.pi()
 
 '''
-    pi.setmode(GPIO_ pin number, IN/OUT)
-
- '''
+pi.setmode(GPIO_ pin number, IN/OUT)
+'''
 
 pi.set_mode(13,pigpio.OUTPUT)  #PWMA 13         7
 pi.set_mode(17,pigpio.OUTPUT)  #AIN2 17       11
 pi.set_mode(18,pigpio.OUTPUT)  #AIN1 18        12
 pi.set_mode(27,pigpio.OUTPUT)  #STBY 27        13
 
-
-#pi.write(27,0) #   
+#pi.write(27,0) #
 
 counter = 0
 while(counter<=1):
     print(counter)
     counter+=1
 
-    #Clockwise direction 
+    #Clockwise direction
     # print "Clockwise direction"
     pi.write(27,1) #disable standby active low
     pi.write(18,1)
@@ -46,9 +44,9 @@ while(counter<=1):
     pi.set_PWM_dutycycle(13,125)
     #pi.hardware_PWM(13, 800, 400000) # 800Hz 75% dutycycle
     time.sleep(2)
-   
 
-    
+
+
     #Anti Clockwise direction
     #print "Anti Clockwise direction"
     #pi.write(27,1)
